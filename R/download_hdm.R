693bfc743baf54156727faee91d8b722cb6cafac
Rafael Schouten <rafaelschouten@gmail.com>
2021-05-20 20:51:13 +1000

    start hdm download script

---
+++ R/download_hdm.R
@@ -0,0 +1,5 @@
+
+# "https://maps2.biodiversity.vic.gov.au/Models/SMP_Dromaius%20novaehollandiae_Emu_10001.zip"
+
+str_split(species)
+url = paste("https://maps2.biodiversity.vic.gov.au/Models/SMP_", genus, "%20", species, "_" common_name, "_", taxon_id, ".zip")
