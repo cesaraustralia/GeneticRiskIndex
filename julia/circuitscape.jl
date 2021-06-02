37f328b014675771ee12ebbf43381f7c985b9f12
Rafael Schouten <rafaelschouten@gmail.com>
2021-06-02 14:06:42 +1000

    write mode

--- julia/circuitscape.jl
+++ julia/circuitscape.jl
@@ -7,6 +7,6 @@ symlink(joinpath(homedir(), "taxon_data"), "data")
 seconds_elapsed = @elapsed compute("circuitscape_model.ini")
 
 # Store the time taken to run it
-open("data/seconds_elapsed.txt", "a") do io
+open("data/seconds_elapsed.txt", "w") do io
    println(io, seconds_elapsed)
 end
