09f30424cdcf900c36aa050fe954906f201192b9
Rafael Schouten <rafaelschouten@gmail.com>
2021-06-02 13:22:39 +1000

    store timings from circuitscape

--- julia/circuitscape.jl
+++ julia/circuitscape.jl
@@ -1,7 +1,12 @@
 using Circuitscape
 
-# Symlin taxon data folder to this directory
+# Symlink taxon data folder to this directory
 symlink(joinpath(homedir(), "taxon_data"), "data")
 
 # Run the model
-compute("circuitscape_model.ini")
+seconds_elapsed = @elapsed compute("circuitscape_model.ini")
+
+# Store the time taken to run it
+open("data/seconds_elapsed.txt", "a") do io
+   println(io, seconds_elapsed)
+end
