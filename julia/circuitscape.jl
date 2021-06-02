0ed30d1a8eb93e9158be0a5b214e6bd9dd4c93c9
Rafael Schouten <rafaelschouten@gmail.com>
2021-06-02 14:15:06 +1000

    use project path

--- julia/circuitscape.jl
+++ julia/circuitscape.jl
@@ -1,13 +1,14 @@
-using Circuitscape
+using Circuitscape, Pkg
 
-dir = dirname(@__FILE__)
+projectdir = dirname(Pkg.project().path)
+
 # Symlink taxon data folder to this directory
-if !isdir(joinpath(dir, "data"))
-    symlink(joinpath(homedir(), "taxon_data"), joinpath(dir, "data"))
+if !isdir(joinpath(projectdir, "data"))
+    symlink(joinpath(homedir(), "taxon_data"), joinpath(projectdir, "data"))
 end
 
 # Run the model
-seconds_elapsed = @elapsed compute(joinpath(dir, "circuitscape_model.ini"))
+seconds_elapsed = @elapsed compute(joinpath(projectdir, "circuitscape_model.ini"))
 
 # Store the time taken to run it
 open("data/seconds_elapsed.txt", "w") do io
