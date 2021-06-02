6977c6db25c60bcf7ee9688e56bd37c360ffbe27
Rafael Schouten <rafaelschouten@gmail.com>
2021-06-02 14:12:57 +1000

    set dir

--- julia/circuitscape.jl
+++ julia/circuitscape.jl
@@ -1,12 +1,13 @@
 using Circuitscape
 
+dir = dirname(@__FILE__)
 # Symlink taxon data folder to this directory
-if !isdir("data")
-    symlink(joinpath(homedir(), "taxon_data"), "data")
+if !isdir(joinpath(dir, "data"))
+    symlink(joinpath(homedir(), "taxon_data"), joinpath(dir, "data"))
 end
 
 # Run the model
-seconds_elapsed = @elapsed compute("circuitscape_model.ini")
+seconds_elapsed = @elapsed compute(joinpath(dir, "circuitscape_model.ini"))
 
 # Store the time taken to run it
 open("data/seconds_elapsed.txt", "w") do io
