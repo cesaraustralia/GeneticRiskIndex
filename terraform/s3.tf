9fa6ce223a68adc0135ff5de763ad0a77378c9f7
Rafael Schouten <rafaelschouten@gmail.com>
2021-07-15 15:03:11 +1000

    add allow get to s3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,7 +3,7 @@ data "aws_s3_bucket" "bucket" {
 }
 
 # See https://aws.amazon.com/blogs/security/writing-iam-policies-how-to-grant-access-to-an-amazon-s3-bucket/
- 
+
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = data.aws_s3_bucket.bucket.id
   policy = <<POLICY
@@ -16,62 +16,73 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       "Principal": "*",
       "Action": ["s3:GetObject"],
       "Resource": "${data.aws_s3_bucket.bucket.arn}/*"
-    },
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": [
-        "s3:PutObject", 
-        "s3:GetObject", 
-        "s3:DeleteObject"
-      ],
-      "Resource": [
-        "${data.aws_s3_bucket.bucket.arn}",
-        "${data.aws_s3_bucket.bucket.arn}/*",
-        "${data.aws_s3_bucket.bucket.arn}/job/*"
-      ]
-    }
-  ]
+     },
+     {
+       "Sid": "",
+       "Effect": "Allow",
+       "Principal": "*",
+       "Action": [
+         "s3:PutObject", 
+         "s3:GetObject", 
+         "s3:DeleteObject"
+       ],
+       "Resource": [
+         "${data.aws_s3_bucket.bucket.arn}",
+         "${data.aws_s3_bucket.bucket.arn}/*",
+         "${data.aws_s3_bucket.bucket.arn}/job/*"
+         ]
+       },
+       {
+         "Effect": "Allow",
+         "Principal": "*",
+         "Action": "s3:GetObject",
+         "Resource": "arn:aws:s3:::${data.aws_s3_bucket.bucket.arn}/*",
+         "Condition": {
+           "StringEquals": {
+             "s3:ExistingObjectTag/public": "yes"
+           }
+         }
+       }
+     ]
 }
-POLICY
+  POLICY
 }
 
-# resource "aws_s3_bucket_policy" "bucket_user_policy" {
-#   bucket = data.aws_s3_bucket.bucket.id
-#   policy = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": [
-#         "s3:GetBucketLocation",
-#         "s3:ListAllMyBuckets"
-#       ],
-#       "Resource": "*"
-#     },
-#     {
-#       "Effect": "Allow",
-#       "Action": ["s3:ListBucket"],
-#       "Resource": ["${data.aws_s3_bucket.bucket.arn}"]
-#     },
-#     {
-#       "Effect": "Allow",
-#       "Action": [
-#         "s3:PutObject",
-#         "s3:GetObject",
-#         "s3:DeleteObject"
-#       ],
-#       "Resource": ["${data.aws_s3_bucket.bucket.arn}/*"]
-#     }
-#   ]
-# }
-# POLICY
-# }
+    # resource "aws_s3_bucket_policy" "bucket_user_policy" {
+    #   bucket = data.aws_s3_bucket.bucket.id
+    #   policy = <<POLICY
+    # {
+    #   "Version": "2012-10-17",
+    #   "Statement": [
+    #     {
+    #       "Effect": "Allow",
+    #       "Action": [
+    #         "s3:GetBucketLocation",
+    #         "s3:ListAllMyBuckets"
+    #       ],
+    #       "Resource": "*"
+    #     },
+    #     {
+    #       "Effect": "Allow",
+    #       "Action": ["s3:ListBucket"],
+    #       "Resource": ["${data.aws_s3_bucket.bucket.arn}"]
+    #     },
+    #     {
+    #       "Effect": "Allow",
+    #       "Action": [
+    #         "s3:PutObject",
+    #         "s3:GetObject",
+    #         "s3:DeleteObject"
+    #       ],
+    #       "Resource": ["${data.aws_s3_bucket.bucket.arn}/*"]
+     #     }
+     #   ]
+     # }
+     # POLICY
+     # }
 
-output "s3-bucket-url" {
-  description = "URL for the s3 bucket"
-  value = "https://${data.aws_s3_bucket.bucket.bucket_regional_domain_name}"
-}
+     output "s3-bucket-url" {
+       description = "URL for the s3 bucket"
+       value = "https://${data.aws_s3_bucket.bucket.bucket_regional_domain_name}"
+     }
 
